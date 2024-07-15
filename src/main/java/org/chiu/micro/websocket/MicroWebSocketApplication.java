488965e519cdcb5e451d779ce6e229111909e1d1
mingchiuli <mingchiuli@163.com>
2024-07-16 07:47:56 +0800

    test

--- src/main/java/org/chiu/micro/websocket/MicroWebSocketApplication.java
+++ src/main/java/org/chiu/micro/websocket/MicroWebSocketApplication.java
@@ -3,10 +3,11 @@ package org.chiu.micro.websocket;
 import org.chiu.micro.websocket.config.CustomRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class }, proxyBeanMethods = false)
 @ImportRuntimeHints({ CustomRuntimeHints.class })
 public class MicroWebSocketApplication {
 
