6b60a6a814da09ac09a0ef8805a2d84919c4207a
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-07-16 10:35:22 +0800

    Update AuthHttpService.java

--- src/main/java/org/chiu/micro/websocket/rpc/AuthHttpService.java
+++ src/main/java/org/chiu/micro/websocket/rpc/AuthHttpService.java
@@ -2,7 +2,7 @@ package org.chiu.micro.websocket.rpc;
 
 import org.chiu.micro.websocket.lang.Result;
 
-import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 
@@ -10,6 +10,6 @@ import org.springframework.web.service.annotation.GetExchange;
 public interface AuthHttpService {
 
     @GetExchange("/auth/{token}")
-    Result<Authentication> getAuthentication(@PathVariable String token);
+    Result<PreAuthenticatedAuthenticationToken> getAuthentication(@PathVariable String token);
 
 }
