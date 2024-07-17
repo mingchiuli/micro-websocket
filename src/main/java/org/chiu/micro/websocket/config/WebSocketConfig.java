e1bb0bd011657d77a6baed017241aea64a806650
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-07-17 09:21:44 +0800

    Update WebSocketConfig.java

--- src/main/java/org/chiu/micro/websocket/config/WebSocketConfig.java
+++ src/main/java/org/chiu/micro/websocket/config/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/edit/ws")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("https://chiu.wiki");
     }
 
     @Override
