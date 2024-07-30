47cbf7d6979a67c27789dbe78524c3bb435cb3f1
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-07-30 15:09:17 +0800

    Update MicroWebSocketApplication.java

--- src/main/java/org/chiu/micro/websocket/MicroWebSocketApplication.java
+++ src/main/java/org/chiu/micro/websocket/MicroWebSocketApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ImportRuntimeHints;
-
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class }, proxyBeanMethods = false)
 @ImportRuntimeHints({ CustomRuntimeHints.class })
+@EnableMethodSecurity
 public class MicroWebSocketApplication {
 
 	public static void main(String[] args) {
