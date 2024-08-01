3e2fdc51240071fc0a26b0bc7917d4062e25b692
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-08-01 15:52:41 +0800

    Update WebSocketSecurityConfig.java

--- src/main/java/org/chiu/micro/websocket/config/WebSocketSecurityConfig.java
+++ src/main/java/org/chiu/micro/websocket/config/WebSocketSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.security.authorization.AuthorizationManager;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 
 /**
@@ -14,7 +13,6 @@ import org.springframework.security.messaging.access.intercept.MessageMatcherDel
  * @create 2023-01-15 11:38 am
  */
 @Configuration
-@EnableWebSocketSecurity
 public class WebSocketSecurityConfig {
 
     @Bean
